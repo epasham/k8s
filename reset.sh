85fac146c310dfc6d13efbc4c7d5cf0e41639d60
root <root@master01.lcepj0re31yubbkjd3wg1504lf.dx.internal.cloudapp.net>
2019-02-28 11:48:44 +0000

    initial commit

---
+++ reset.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+kubeadm reset
+rm -rf $HOME/.kube
+kubeadm init --pod-network-cidr=10.244.0.0/16
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+export KUBECONFIG=/etc/kubernetes/admin.conf
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
